c9e9dc79961422bbef3dbf9c6154f60f63cac8d6
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-01-04 16:08:24 -0800

    playground

--- playground.rb
+++ playground.rb
@@ -1,8 +1,18 @@
-puts 'what is your first name??'
+lineWidth = 50
+
+puts( 'what is your first name??'.center(lineWidth))
 first = gets.chomp
 
-puts 'what is your last name?'
+puts( 'what is your last name?'.center(lineWidth))
 last = gets.chomp
 
-puts 'did you know there are ' + first.length.to_s + ' letters in your first name, ' + first + '?'
-puts 'and there are ' + last.length.to_s + ' letters in your last name, ' + first + ' ' + last + '.'
+lineWidth = 1
+puts( 'did you know there are ' + first.length.to_s + ' letters in your first name, ' + first + '?'.center(lineWidth))
+puts( 'and there are ' + last.length.to_s + ' letters in your last name, ' + first + ' ' + last + '.'.center(lineWidth))
+
+lineWidth = 40
+str = '--> text <--'
+puts str.ljust lineWidth
+puts str.center lineWidth
+puts str.rjust lineWidth
+puts str.ljust(lineWidth/2) + str.rjust(lineWidth/2)
