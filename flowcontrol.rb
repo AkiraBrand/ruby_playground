b76ffb67c0f6496daa8670cc0fd63b8c803922cf
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-01-05 21:21:53 -0800

    adds flowcontrol practice file

---
+++ flowcontrol.rb
@@ -0,0 +1,21 @@
+# puts 1 > 2
+# puts 1 < 2
+#
+# puts 5 >= 5
+# puts 5<= 4
+#
+# puts 1 == 1
+# puts 2 != 1
+#
+# #compare dictionary ordering
+# #capitals always come before lowercase letters
+# puts 'cat' < 'dog'
+
+puts 'I am a fortune-teller.  Tell me your name:'
+name = gets.chomp
+if name == 'Chris'
+  puts 'I see great things in your future.'
+else
+  puts 'Your future is... Oh my!  Look at the time!'
+  puts 'I really have to go, sorry!'
+end
