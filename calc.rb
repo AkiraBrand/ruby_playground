da2943806f8a4337be313cd0626961e510058af3
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-01-05 21:11:43 -0800

    adds math module calculations

--- calc.rb
+++ calc.rb
@@ -58,3 +58,10 @@ puts ''
 var1 = 'eight'
 puts var1
 puts var2
+puts(Math::PI)
+puts(Math::E)
+
+puts(Math.cos(Math::PI/3))
+puts(Math.tan(Math::PI/4))
+puts(Math.log(Math::E**2))
+puts((1 + Math.sqrt(5))/2)
