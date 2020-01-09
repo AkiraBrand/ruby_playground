915e68d295dda68b94596c11845344f7ac650a9e
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-01-08 19:01:27 -0800

    extension one wip

--- deaf_grandma.rb
+++ deaf_grandma.rb
@@ -1,13 +1,13 @@
-# Write a Deaf Grandma program. Whatever you say to grandma (whatever you type in), she should respond with HUH?!  SPEAK UP, SONNY!,
-# unless you shout it (type in all capitals). If you shout, she can hear you (or at least she thinks so) and yells back, NO, NOT SINCE 1938!
-#To make your program really believable, have grandma shout a different year each time; maybe any year at random between 1930 and 1950.
-# (This part is optional, and would be much easier if you read the section on Ruby's random number generator at the end of the methods chapter.)
-# You can't stop talking to grandma until you shout BYE.
-# Hint: Don't forget about chomp! 'BYE'with an Enter is not the same as 'BYE' without one!
-# Hint 2: Try to think about what parts of your program should happen over and over again. All of those should be in your while loop.
+# Extend your Deaf Grandma program: What if grandma doesn't want you to leave?
+# When you shout BYE, she could pretend not to hear you.
+# Change your previous program so that you have to shout BYE three times in a row.
+# Make sure to test your program: if you shout BYE three times, but not in a row,
+# you should still be talking to grandma.
+#
 puts 'Say something to your grandma!'
 
 reply = gets.chomp
+reply_times = []
 
 while reply == reply.downcase
   grandma_one = "HUH?! SPEAK UP, SONNY!"
@@ -23,5 +23,15 @@ while reply == reply.upcase && reply != "BYE"
 end
 
 if reply == "BYE"
-  puts "GOODBYE, MY DEAR GRANDCHILD!"
+  reply_times << reply
+  while reply_times.count <= 3
+    puts "..."
+    if reply_times.count > 3
+      puts "OK FINE BYE"
+    end
+  end
 end
+
+
+#i could say that if user input elements x in an array [0..2] are all equal, then exit the program
+#else continue
