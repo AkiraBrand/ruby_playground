43ce581feca135d81e6cc66c13749caaf92d7b90
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-01-10 21:07:26 -0800

    wip

--- deaf_grandma.rb
+++ deaf_grandma.rb
@@ -9,29 +9,32 @@ puts 'Say something to your grandma!'
 reply = gets.chomp
 reply_times = []
 
-if reply == reply.downcase
-  grandma_one = "HUH?! SPEAK UP, SONNY!"
-  puts grandma_one
-  reply = gets.chomp
+# if reply == reply.downcase
+#   grandma_one = "HUH?! SPEAK UP, SONNY!"
+#   puts grandma_one
+#   reply = gets.chomp
+# end
+
+def upcase(reply)
+  if reply == reply.upcase && reply != "BYE"
+    grandma_two = "NO, NOT SINCE " + (rand(1930..1950)).to_s + "!"
+    puts grandma_two
+    reply = gets.chomp
+  elsif reply == reply.downcase
+    grandma_one = "HUH?! SPEAK UP, SONNY!"
+    puts grandma_one
+    reply = gets.chomp
+  end
 end
 
-
-if reply == reply.upcase && reply != "BYE"
-  grandma_two = "NO, NOT SINCE " + (rand(1930..1950)).to_s + "!"
-  puts grandma_two
-  reply = gets.chomp
-end
-
-while reply == "BYE"
+if reply == "BYE"
   reply_times << reply
-  #if the times i've said bye is three, break out of the program
-  # reply_times << reply
-  # # require 'pry'; binding.pry
-  # while reply_times.count <= 4
-    puts "..."
     if reply_times.count == 3
       puts "ok, bye"
-      break
+    else
+      puts "oh no, you're not leaving yet!"
+      reply = gets.chomp
+      upcase(reply)
     end
   end
 
