d784eaa3db87637b011b34bf8993368a5eb30f59
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-01-14 22:23:53 -0800

    further refactor

--- deaf_grandma.rb
+++ deaf_grandma.rb
@@ -13,6 +13,11 @@ until input == input.upcase
   input = gets.chomp
 end
 
-if input == input.upcase
+while input == input.upcase && input != "BYE"
   puts "NO, NOT SINCE " + (rand(1965..1999)).to_s + "!"
+  input = gets.chomp
+end
+
+if input == "BYE"
+  puts "OK BYE DEAR"
 end
