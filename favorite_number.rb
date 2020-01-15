fdb6d618b0c60821ece775896398a55f2de460b4
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-01-14 22:15:57 -0800

    solves favorite number

--- favorite_number.rb
+++ favorite_number.rb
@@ -2,3 +2,9 @@
 #  Have your program add one to the number, then suggest the result
 #  as a bigger and better favorite number.
 #  (Do be tactful about it, though.)
+
+puts "What is your favorite number?"
+
+favorite = gets.chomp
+
+puts "Hm, " + favorite + " is a good number. But what about " + (favorite + (rand(1..100).to_s)) + "?"
