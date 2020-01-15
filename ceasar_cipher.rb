6d9c8412768eb746b18eb1cc46d62acac684897a
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-01-14 22:37:31 -0800

    pseudocode first method

---
+++ ceasar_cipher.rb
@@ -0,0 +1,17 @@
+#take a string and a shift factor then ouput the string shifted
+
+#i'll need to have a method that turns the string into a number
+
+#wrap from a to z
+
+#and a shift method
+
+
+def translate_string_to_number(input)
+
+  #take the input
+  #break it up into individual letters
+  #map the letters to a dictionary (a = 1) or
+  #map the input to a placement in an array
+
+end
