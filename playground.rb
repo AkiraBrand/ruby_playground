c4003c307ce6c8130fecfd73f8dcc1c6219ed3f0
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-01-16 19:49:51 -0800

    pop length push

--- playground.rb
+++ playground.rb
@@ -1,13 +1,9 @@
-foods = ['artichoke', 'brioche', 'caramel']
-
-puts foods
-puts
-puts foods.to_s
-
-puts foods.join(', ')
-puts
-puts foods.join(' :) ') + ' 8)'
-
-200.times do
-  puts [[' ']]
-end
+favorites = []
+favorites.push 'raindrops on roses'
+favorites.push 'whiskers on kittens'
+puts favorites.join(', ')
+puts favorites[0]
+puts favorites.length
+puts favorites.pop
+puts favorites
+puts favorites.length
