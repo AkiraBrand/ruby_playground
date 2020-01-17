da6f27708ed5ae30e49ace8c927e8ec38ebc433e
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-01-16 19:47:01 -0800

    join practice

--- playground.rb
+++ playground.rb
@@ -1,4 +1,13 @@
 foods = ['artichoke', 'brioche', 'caramel']
 
 puts foods
-puts 
+puts
+puts foods.to_s
+
+puts foods.join(', ')
+puts
+puts foods.join(' :) ') + ' 8)'
+
+200.times do
+  puts [[' ']]
+end
