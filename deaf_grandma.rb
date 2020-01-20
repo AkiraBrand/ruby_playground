f78670a9181c48ac037d8fb4757c93866797eb9a
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-01-19 19:04:06 -0800

    iteration 1 complete

--- deaf_grandma.rb
+++ deaf_grandma.rb
@@ -20,12 +20,12 @@ bye_counter = 0
 
 until bye_counter == 3
   input = gets.chomp
-  if input == input.capitalize
+  if input == "BYE"
+    bye_counter += 1
+  elsif input == input.upcase && input != "BYE"
     puts "NO, NOT SINCE " + (rand(1965..1999)).to_s + "!"
-  elsif input == "BYE"
-      bye_counter += 1
   elsif input == input.downcase
-      puts "HUH? SPEAK UP SONNY!"
+    puts "HUH? SPEAK UP SONNY!"
   end
 end
 
